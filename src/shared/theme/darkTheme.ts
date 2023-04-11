13eb91fb234cc9147ce510de514bcff782a05557
Davi Alves <cadwvk@gmail.com>
2023-04-11 18:56:44 -0300

    theme created

---
+++ src/shared/theme/darkTheme.ts
@@ -0,0 +1,48 @@
+export const darkTheme = {
+  bg: {
+    primary: '#1D203E',
+    secondary: '#2C2F48',
+    tertiary: '#292C45',
+    bg_linear: 'linear-gradient(6.94deg, #19004F -39.97%, #8B68F0 92.16%)',
+    bg_sidebar: 'rgba(44, 47, 72, 0.2)',
+  },
+  sidebarButtons: {
+    default: 'rgba(0, 0, 0, 0.0)',
+    hover: 'rgba(0, 0, 0, 0.5)',
+    selected: 'rgba(0, 0, 0, 0.25)',
+    selected_default: 'rgba(0, 0, 0, 0.0)',
+  },
+  text: {
+    h1: 'linear-gradient(270deg, #48A7FF 29.3%, #00FFF0 100%)',
+    primary: '#FFF',
+    secondary: '#A9B1FF',
+    tertiary: 'rgba(255, 255, 255, 0.25)',
+    quaternary: 'rgba(255, 255, 255, 0.5)',
+    fifth: '#CDCEE0',
+  },
+  alert: {
+    primary: '#1C1D30',
+    secondary: '#212237',
+    tertiary: '#161831',
+  },
+  caixas: {
+    default: 'rgba(255, 255, 255, 0)',
+    hover: 'rgba(172, 180, 255, 0.06)',
+    primary: '#3C4071',
+  },
+  elements: {
+    primary: '#2E3153',
+    secondary: 'rgba(46, 49, 83, 0.25)',
+  },
+  icons: {
+    selected: 1,
+    default: 0.6,
+    hover_bg: 'rgba(255, 255, 255, 0.1)',
+  },
+  buttons: {
+    bg_default_primary: '#212237',
+    bg_default_secondary: '#484C6C',
+    hover: '#141425',
+    border_focus_visible: '#4F5179;',
+  },
+};
