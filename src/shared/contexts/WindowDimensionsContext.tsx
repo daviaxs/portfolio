ca84041d09dff9bbf22397d540d9adcba0bb037d
Davi Alves <cadwvk@gmail.com>
2023-04-12 16:04:31 -0300

    created WindowDimensionsContext

---
+++ src/shared/contexts/WindowDimensionsContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react"
+
+interface IWindowDimensionsContextProps {
+  width: number
+  height: number
+}
+
+interface IWindowDimensionsProviderProps {
+  children: React.ReactNode
+}
+
+export const WindowDimensionsContext =
+  createContext<IWindowDimensionsContextProps>(
+    {} as IWindowDimensionsContextProps
+  )
+
+export const WindowDimensionsProvider: React.FC<
+  IWindowDimensionsProviderProps
+> = ({ children }) => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <WindowDimensionsContext.Provider value={windowDimensions}>
+      {children}
+    </WindowDimensionsContext.Provider>
+  )
+}
