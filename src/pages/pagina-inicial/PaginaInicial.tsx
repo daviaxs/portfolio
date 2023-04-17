219080a882b35b0efa7e8bde7a67ed7a356c4ecd
Davi Alves <cadwvk@gmail.com>
2023-04-17 12:54:07 -0300

    Homepage created

--- src/pages/pagina-inicial/PaginaInicial.tsx
+++ src/pages/pagina-inicial/PaginaInicial.tsx
@@ -1,18 +1,49 @@
+import { useContext } from "react"
+
+import { WindowDimensionsContext } from "../../shared/contexts/WindowDimensionsContext"
 import { Container } from "../../shared/components/container/Container"
+import { THeadingPrimary, TTitlePrimary } from "../../shared/fonts"
 import { LayoutBaseDePagina } from "../../shared/layout"
 import Imgs from "../../shared/assets/index"
-import { THeadingPrimary } from "../../shared/fonts"
 import { theme } from "../../shared/theme"
 
 export const PaginaInicial = () => {
+  const { width: windowWidth } = useContext(WindowDimensionsContext)
+
   return (
-    <LayoutBaseDePagina flexDir="row">
-      <Container display="flex" height="100%" width="100%" align="center" justifyContent="space-between">
-        <THeadingPrimary txtColor={theme.text.h1} txtShadow="2px 2px 0px #3C427B">
+    <LayoutBaseDePagina flexDir={windowWidth <= 900 ? "column" : "row"}>
+      <Container
+        display="flex"
+        height=""
+        width=""
+        marginTop={windowWidth <= 900 ? 2 : 12}
+        marginLeft={windowWidth <= 600 ? 0 : 1.5}
+        marginRight={windowWidth <= 900 ? 0 : windowWidth / 200}
+        flexDir="column"
+        align={windowWidth <= 900 ? "center" : "start"}
+      >
+        <TTitlePrimary
+          txtColor={theme.text.secondary}
+          fontSize={windowWidth >= 900 ? windowWidth / 590 : windowWidth <= 600 ? 1.5 : 2}
+        >
+          Olá, eu sou
+        </TTitlePrimary>
+        <THeadingPrimary
+          txtColor={theme.text.h1}
+          txtShadow="2px 2px 0px #3C427B"
+          whiteSpace="nowrap"
+          fontSize={windowWidth >= 900 ? windowWidth / 200 : windowWidth <= 600 ? 4.5 : 6}
+        >
           Davi Alves
         </THeadingPrimary>
-        <img src={Imgs.AvatarImage} alt="" style={{transform: "scale(.45)"}} />
+        <TTitlePrimary
+          txtColor={theme.text.secondary}
+          fontSize={windowWidth >= 900 ? windowWidth / 590 : windowWidth <= 600 ? 1.5 : 2}
+        >
+          Desenvolvedor front-end
+        </TTitlePrimary>
       </Container>
+      <img src={Imgs.AvatarImage} alt="Desenho de davi alves" />
     </LayoutBaseDePagina>
   )
 }
