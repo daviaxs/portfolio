a0bf9dda3e0f3dccee4296c87baa440d99737085
Davi Alves <cadwvk@gmail.com>
2023-04-25 18:00:50 -0300

    finish card component

--- src/pages/sobre/utils/Card.tsx
+++ src/pages/sobre/utils/Card.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import styled from "@emotion/styled"
+import { Skeleton, Stack } from "@mui/material"
 
+import { THeadingLinearPrimary } from "../../../shared/fonts"
 import { theme, themeConstants } from "../../../shared/theme"
 import Imgs from "../../../shared/assets/index"
-import { useState } from "react"
-import { Skeleton, Stack } from "@mui/material"
+import { Container } from "../../../shared/components/container/Container"
 
 const CardStyle = styled.div`
   display: flex;
@@ -11,17 +13,25 @@ const CardStyle = styled.div`
   justify-content: start;
 
   width: 25rem;
-  height: 6rem;
+  gap: ${themeConstants.sizes["4xl"]}rem;
   padding: ${themeConstants.sizes.lg}rem ${themeConstants.sizes["2xl"]}rem;
 
   background: ${theme.alert.tertiary};
-  border-radius: ${themeConstants.sizes["5xl"]}rem;
+  border-radius: ${themeConstants.sizes["4xl"]}rem;
+  border: 1px solid ${theme.buttons.bg_default_secondary};
 
   .avatar {
-    height: 4.613rem;
+    height: 4.5rem;
 
     border-radius: 999px;
     border: 2px solid #44476a;
+  }
+
+  .line {
+    width: 7rem;
+    height: 0.5rem;
+    background: ${theme.bg.secondary};
+    border-radius: 2rem;
   }
 `
 
@@ -33,11 +43,15 @@ export const Card = () => {
       <Stack>
         {imgLoading ? (
           <img src={Imgs.ProfilePicture} alt="Foto de perfil" className="avatar" />
-          ) : (
-          <Skeleton variant="circular" style={{ width: "4.613rem", height: "4.613rem", background: "#292f67a1" }} />
+        ) : (
+          <Skeleton variant="circular" style={{ width: "4.5rem", height: "4.5rem", background: "#292f67a1" }} />
         )}
         <img src={Imgs.ProfilePicture} alt="Foto de perfil" className="avatar" onLoad={() => setImgLoading(true)} style={{ display: "none" }} />
       </Stack>
+      <Container display="flex" flexDir="column" height="" width="">
+        <THeadingLinearPrimary fontSize={themeConstants.sizes["6xl"]}>Sobre mim</THeadingLinearPrimary>
+        <span className="line"></span>
+      </Container>
     </CardStyle>
   )
 }
