282b7dd546f622fb8487426ba95223d1dbacdd7a
Davi Alves <cadwvk@gmail.com>
2023-04-25 18:29:56 -0300

    added design responsive in CardProfile

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,18 +1,22 @@
 import { useContext } from "react"
 
 import { WindowDimensionsContext } from "../../shared/contexts/WindowDimensionsContext"
-import { Container } from "../../shared/components/container/Container"
 import { LayoutBaseDePagina } from "../../shared/layout"
-import { theme, themeConstants } from "../../shared/theme"
-import { Card } from "./utils/Card"
-
+import { themeConstants } from "../../shared/theme"
+import { CardProfile } from "./utils/CardProfile"
 
 export const Sobre = () => {
   const { width: windowWidth, height: windowHeight } = useContext(WindowDimensionsContext)
 
   return (
     <LayoutBaseDePagina flexDir="column" paddingInline={themeConstants.sizes["5xl"]}>
-      <Card/>
+      <CardProfile
+        flexDir={windowWidth <= 350 ? "column" : "row"}
+        label="Sobre mim"
+        width={windowWidth <= 600 ? "100%" : "25rem"}
+        fontSize={windowWidth <= 380 ? 2 : 2.5}
+        lineWidth={windowWidth <= 380 ? 5 : 7}
+      />
     </LayoutBaseDePagina>
   )
 }
--- src/pages/sobre/utils/CardProfile.tsx
+++ src/pages/sobre/utils/CardProfile.tsx
@@ -2,17 +2,26 @@ import { useState } from "react"
 import styled from "@emotion/styled"
 import { Skeleton, Stack } from "@mui/material"
 
+import { Container } from "../../../shared/components/container/Container"
 import { THeadingLinearPrimary } from "../../../shared/fonts"
 import { theme, themeConstants } from "../../../shared/theme"
 import Imgs from "../../../shared/assets/index"
-import { Container } from "../../../shared/components/container/Container"
 
-const CardStyle = styled.div`
+interface ICardProfileProps {
+  width: string
+  label: string
+  fontSize: number
+  lineWidth?: number
+  flexDir?: "row" | "column"
+}
+
+const CardProfileStyle = styled.div<ICardProfileProps>`
   display: flex;
+  flex-direction: ${(props) => props.flexDir};
   align-items: center;
   justify-content: start;
 
-  width: 25rem;
+  width: ${(props) => props.width};
   gap: ${themeConstants.sizes["4xl"]}rem;
   padding: ${themeConstants.sizes.lg}rem ${themeConstants.sizes["2xl"]}rem;
 
@@ -28,18 +37,18 @@ const CardStyle = styled.div`
   }
 
   .line {
-    width: 7rem;
+    width: ${(props) => props.lineWidth}rem;
     height: 0.5rem;
     background: ${theme.bg.secondary};
     border-radius: 2rem;
   }
 `
 
-export const Card = () => {
+export const CardProfile: React.FC<ICardProfileProps> = ({ flexDir, width = "25rem", label = "Label", fontSize, lineWidth = 7 }) => {
   const [imgLoading, setImgLoading] = useState(false)
 
   return (
-    <CardStyle>
+    <CardProfileStyle flexDir={flexDir} width={width} label={label} fontSize={fontSize} lineWidth={lineWidth}>
       <Stack>
         {imgLoading ? (
           <img src={Imgs.ProfilePicture} alt="Foto de perfil" className="avatar" />
@@ -49,9 +58,11 @@ export const Card = () => {
         <img src={Imgs.ProfilePicture} alt="Foto de perfil" className="avatar" onLoad={() => setImgLoading(true)} style={{ display: "none" }} />
       </Stack>
       <Container display="flex" flexDir="column" height="" width="">
-        <THeadingLinearPrimary fontSize={themeConstants.sizes["6xl"]}>Sobre mim</THeadingLinearPrimary>
+        <THeadingLinearPrimary fontSize={fontSize} whiteSpace="nowrap">
+          {label}
+        </THeadingLinearPrimary>
         <span className="line"></span>
       </Container>
-    </CardStyle>
+    </CardProfileStyle>
   )
 }
