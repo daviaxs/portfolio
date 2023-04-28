36e6eca28535213c16caab86ca745810f353b256
Davi Alves <cadwvk@gmail.com>
2023-04-28 14:43:00 -0300

    Created projects page

--- src/pages/projetos/Projetos.tsx
+++ src/pages/projetos/Projetos.tsx
@@ -4,7 +4,7 @@ import { themeConstants } from "../../shared/theme"
 export const Projetos = () => {
   return (
     <LayoutBaseDePagina flexDir="column" paddingInline={themeConstants.sizes["5xl"]} paddingTop={themeConstants.sizes["5xl"]}>
-      Hello world
+      projetos
     </LayoutBaseDePagina>
   )
 }
