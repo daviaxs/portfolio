21204322957c99ffe2c1ce03ab25f14ec6ac4f20
Davi Alves <cadwvk@gmail.com>
2023-04-30 17:54:35 -0300

    Added skeleton in styled components logo

--- src/shared/assets/logos-tech/styledComponentsLogo.tsx
+++ src/shared/assets/logos-tech/styledComponentsLogo.tsx
@@ -1,8 +1,25 @@
-import styledcomponentsImg from './styledComponentsLogo.png'
+import { useState } from "react"
+import { Skeleton, Stack } from "@mui/material"
+
+import styledcomponentsImg from "./styledComponentsLogo.png"
 
 const styledcomponentsLogo = ({ size }: { size: number }) => {
+  const [imgLoading, setImgLoading] = useState(false)
+
   return (
-    <img src={styledcomponentsImg} alt="Styled components logo" style={{height: `${size}rem`, width: `${size}rem`, borderRadius: "0.625rem"}}/>
+    <Stack>
+      {imgLoading ? (
+        <img src={styledcomponentsImg} alt="Styled components logo" style={{ height: `${size}rem`, width: `${size}rem`, borderRadius: "0.625rem" }} />
+      ) : (
+        <Skeleton variant="rounded" className="image" style={{ background: "#292f67a1", height: `${size}rem`, width: `${size}rem`, borderRadius: "0.625rem" }} />
+      )}
+      <img
+        src={styledcomponentsImg}
+        alt="Styled components logo"
+        style={{ height: `${size}rem`, width: `${size}rem`, borderRadius: "0.625rem", display: "none" }}
+        onLoad={() => setImgLoading(true)}
+      />
+    </Stack>
   )
 }
 
