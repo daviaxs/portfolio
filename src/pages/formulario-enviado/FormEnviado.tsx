f6a6e4eb0ed422e345c115ae1a65e3da5dd77716
Davi Alves <cadwvk@gmail.com>
2023-05-04 18:36:47 -0300

    Updated FormSend page

--- src/pages/formulario-enviado/FormEnviado.tsx
+++ src/pages/formulario-enviado/FormEnviado.tsx
@@ -1,10 +1,27 @@
-import { Container } from "../../shared/components/container/Container"
+import styled from "styled-components"
 
+import { themeConstants } from "../../shared/theme"
+import Imgs from "../../shared/assets/index"
+
+
+const FormSentStyle = styled.main`
+  display: flex;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-image: url(${Imgs.BgFormSent});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  padding: ${themeConstants.sizes["6xl"]}rem;
+`
 
 export const FormEnviado = () => {
   return (
-    <Container display="flex" width="100vw" height="100vh">
-      Hello world
-    </Container>
+    <FormSentStyle>
+
+    </FormSentStyle>
   )
 }
--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,9 +1,11 @@
 import AvatarImage from './avatarImage.png'
 import WorldImage from './worldImage.png'
 import ProfilePicture from './img001.png'
+import BgFormSent from './bg-form-sent.png'
 
 export default {
   AvatarImage,
   WorldImage,
   ProfilePicture,
+  BgFormSent,
 }
