61a4bc377fa796b5ffe4ceae93b55a8ec0dcb978
Davi Alves <cadwvk@gmail.com>
2023-05-05 17:55:15 -0300

    Elements added on form sent page

--- src/pages/formulario-enviado/FormEnviado.tsx
+++ src/pages/formulario-enviado/FormEnviado.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 
-import { themeConstants } from "../../shared/theme"
+import { theme, themeConstants } from "../../shared/theme"
 import Imgs from "../../shared/assets/index"
-
+import { Container } from "../../shared/components/container/Container"
+import { THeadingLinearPrimary, TTitleSecondary } from "../../shared/fonts"
+import { ButtonSecondary } from "../../shared/components/buttons/ButtonSecondary"
 
 const FormSentStyle = styled.main`
   display: flex;
@@ -16,12 +18,46 @@ const FormSentStyle = styled.main`
   background-size: cover;
 
   padding: ${themeConstants.sizes["6xl"]}rem;
+
+  overflow: hidden;
+
+  .worldImg {
+    position: fixed;
+    bottom: 0%;
+    right: 0;
+
+    height: 35rem;
+  }
+
+  .light {
+    position: fixed;
+    background-color: #c0a1fd;
+    border-radius: 9999px;
+    filter: blur(165px);
+
+    height: 18rem;
+    width: 18rem;
+
+    right: 18rem;
+    bottom: 10rem;
+  }
 `
 
 export const FormEnviado = () => {
   return (
     <FormSentStyle>
+      <img src={Imgs.WorldImage} alt="Imagem ilustrativa do planeta" className="worldImg" />
+      <span className="light"></span>
 
+      <Container display="flex" flexDir="column" width="100%" height="100%">
+        <THeadingLinearPrimary fontSize={2.5}>Formulário entregue com sucesso!</THeadingLinearPrimary>
+        <TTitleSecondary fontSize={1.5} txtColor={theme.text.secondary}>
+          Seu formulário foi entregue com sucesso! Aguarde minha resposta em breve.
+        </TTitleSecondary>
+        <ButtonSecondary>
+          Voltar
+        </ButtonSecondary>
+      </Container>
     </FormSentStyle>
   )
 }
