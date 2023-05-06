7c39ec26ec1aaf942b21463b17c6eb94ea1cb70b
Davi Alves <cadwvk@gmail.com>
2023-05-06 17:36:27 -0300

    Created settings component

---
+++ src/shared/components/settings/Settings.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+import { Container } from "../container/Container"
+import { TTitleSecondary } from "../../fonts"
+import icons from "../../assets/icons"
+import { Icon } from "../icon/Icon"
+import { theme } from "../../theme"
+
+interface ISetting {
+  icon: keyof typeof icons
+  label: string
+  description?: string
+}
+
+interface ISettingsProps {
+  options: ISetting[]
+}
+
+const SettingsStyle = styled.div``
+
+export const Settings: React.FC<ISettingsProps> = ({ options }) => {
+  return (
+    <SettingsStyle>
+      {options.map((option) => (
+        <Container display="flex" flexDir="row" height="3.375" width="100%" bg={theme.alert.secondary}>
+          <Icon name={option.icon} size={22} />
+          <TTitleSecondary fontSize={1.25}>{option.label}</TTitleSecondary>
+          {option.description && <p>{option.description}</p>}
+        </Container>
+      ))}
+    </SettingsStyle>
+  )
+}
