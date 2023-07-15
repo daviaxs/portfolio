88e22de1c4bfbcc3ebd01e7246036d0317e6b8dd
Davi Alves <cadwvk@gmail.com>
2023-07-15 14:07:59 -0300

    Created style base in ButtonModalActions

--- src/shared/components/modal/utils/buttonModal/ButtonModalActions.tsx
+++ src/shared/components/modal/utils/buttonModal/ButtonModalActions.tsx
@@ -1,10 +1,27 @@
+import styled from "styled-components"
+
 interface ButtonModalActionsProps {
   onRedirect: () => void
   onCopy: () => void
+  children: React.ReactNode
 }
 
-export function ButtonModalActions({ onCopy, onRedirect }: ButtonModalActionsProps) {
+const ButtonModalActionsStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const Button = styled.button`
+  
+`
+
+export function ButtonModalActions({ onCopy, onRedirect, children }: ButtonModalActionsProps) {
   return (
-    <></>
+    <ButtonModalActionsStyle>
+      <Button onClick={onCopy}>{children}</Button>
+      <Button onClick={onRedirect}>{children}</Button>
+    </ButtonModalActionsStyle>
   )
 }
