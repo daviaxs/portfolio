032d3447461b48e3355c66ecf72ec672fac1de01
Davi Alves <cadwvk@gmail.com>
2023-07-15 13:29:27 -0300

    added icons in index

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,23 +1,25 @@
-import IconHome from "@/shared/assets/icons/home"
-import IconPeople from "@/shared/assets/icons/person"
-import IconPaint from "@/shared/assets/icons/paint"
 import IconArrowBack from "@/shared/assets/icons/arrow_back"
 import IconArrowClose from "@/shared/assets/icons/arrow_close"
-import IconArrowOpen from "@/shared/assets/icons/arrow_open"
 import IconArrowDown from "@/shared/assets/icons/arrow_down"
+import IconArrowOpen from "@/shared/assets/icons/arrow_open"
 import IconChat from "@/shared/assets/icons/chat"
-import IconOptions from "@/shared/assets/icons/options"
-import IconRocket from "@/shared/assets/icons/rocket"
-import IconXClose from "@/shared/assets/icons/x_close"
-import Github from "@/shared/assets/icons/github"
+import Copy from "@/shared/assets/icons/copy"
 import Discord from "@/shared/assets/icons/discord"
-import ErrorLoading from "@/shared/assets/icons/error_loading"
-import HappyFace from "@/shared/assets/icons/happyFace"
-import Person02 from "@/shared/assets/icons/person02"
 import Email from "@/shared/assets/icons/email"
+import ErrorLoading from "@/shared/assets/icons/error_loading"
+import Github from "@/shared/assets/icons/github"
+import HappyFace from "@/shared/assets/icons/happyFace"
+import IconHome from "@/shared/assets/icons/home"
 import Message from "@/shared/assets/icons/message"
-import World from "@/shared/assets/icons/world"
+import IconOptions from "@/shared/assets/icons/options"
+import IconPaint from "@/shared/assets/icons/paint"
+import IconPeople from "@/shared/assets/icons/person"
+import Person02 from "@/shared/assets/icons/person02"
+import Redirect from "@/shared/assets/icons/redirect"
+import IconRocket from "@/shared/assets/icons/rocket"
 import Settings from "@/shared/assets/icons/settings"
+import World from "@/shared/assets/icons/world"
+import IconXClose from "@/shared/assets/icons/x_close"
 
 export default {
   home: IconHome,
@@ -40,4 +42,6 @@ export default {
   message: Message,
   world: World,
   settings: Settings,
+  copy: Copy,
+  redirect: Redirect,
 }
